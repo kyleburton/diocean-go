37db9dcbff5e07dd591dfb243b84521ec04c264d
Kyle Burton <kyle.burton@gmail.com>
2014-04-06 11:55:27 -0400

    starting on docs

---
+++ doc.go
@@ -0,0 +1,16 @@
+/*
+Package diocean provides a set of types and functions to interact with <a href="https://developers.digitalocean.com/">Digital Ocean's API</a>.
+
+Configuration
+
+Basics
+
+Types and Values
+
+
+*/
+package diocean
+
+/*
+
+*/
